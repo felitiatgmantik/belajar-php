523c61a101c4276809eabde1fdae7d98d7294bc8
Felitia Theona Geofani Mantik <felitiamantik12@gmail.com>
2022-10-24 11:58:01 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Felitia";
+    $umur = 19;
+
+    echo "Nama saya <strong>$nama</strong> Saya berusia <strong>$umur</strong> tahun.<br>" ;
+
+    $namaAdik = "Natalie";
+    $umurAdik = 14;
+
+    echo "Nama adik saya <strong>$namaAdik</strong> Adik saya berusia <strong>$umurAdik</strong> tahun.<br>";
+ 
+    $selisihUmur = $umur - $umurAdik ;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun. "
+
+
+
+?>
+
+
